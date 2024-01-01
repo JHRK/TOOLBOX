e0c543bbf01e603eb7665d7ba91876e62d700392
JHRK <jhrk05@gmail.com>
2024-01-01 00:32:45 -0300

    fonts

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <link href="style.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Anonymous+Pro&family=DM+Sans:opsz,wght@9..40,600&family=Gabarito:wght@600&family=Manrope:wght@700&family=Open+Sans&family=Poppins:wght@600&family=Sono&family=Space+Mono:wght@700&family=Trispace:wght@800&family=Work+Sans&display=swap" rel="stylesheet">
     <title>TLBX</title>
   </head>
   <body class="container-fluid">
